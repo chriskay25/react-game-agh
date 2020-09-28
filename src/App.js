3ddd5bb2797fd306b2714803e38d9d84b46afa93
Chris Kay <ckay84@protonmail.com>
2020-09-28 16:27:02 -0400

    Set initial state of Redux store, make App a class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {reducer} from './reducers/index.js'
 
-
-function App() {
-  return (
-    <div className="App">
-      <h1>This is a React game!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.props.message}</h1>
+      </div>
+    )
+  }
 }
+
+App.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import App from '../App.js';
+
+const initialState = { message: `It's easy to integrate React and Redux` }
+
+function reducer(state = initialState) {
+  return state
+}
+
+export default reducer
