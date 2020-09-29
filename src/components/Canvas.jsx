73ea0529f8e89f80a078b7b7918ce8159c72e5a4
Chris Kay <ckay84@protonmail.com>
2020-09-28 22:40:09 -0400

    Add Sky component, include in Canvas comp and set style for positioning tweak

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import Sky from './Sky'
 
 
 const Canvas = () => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight]
+  const style = {height: '100%', width: '100%', position: 'fixed', top: '0', left: '0'}
   return (
     <svg 
       id="aliens-go-home-canvas" 
-      preserveAspectRatio="xMaxYMax" // forces uniform scaling of canvas and its elements
+      preserveAspectRatio="xMaxYMax slice" // forces uniform scaling of canvas and its elements
       viewBox={viewBox}
+      style={style}
     >
+      <Sky />
       <circle cx={0} cy={0} r={50} />
     </svg>
   )
